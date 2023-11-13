020a953562d3159f888fbf34167a1483ca2f5f9c
kevin swp <thranva@gmail.com>
2023-11-13 18:29:57 +0100

    add comments

--- src/lib/components/modal/index.jsx
+++ src/lib/components/modal/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './modal.scss';
 
+/**
+ * Modal component for displaying a modal dialog.
+ * @param {boolean} isOpen - Determines if the modal is open or closed.
+ * @param {Function} onClose - Function to call when closing the modal.
+ */
 const Modal = ({ isOpen, onClose }) => {
     if (!isOpen) 
     
@@ -10,7 +15,7 @@ const Modal = ({ isOpen, onClose }) => {
         <div className="modal-overlay">
             <div className="modal-content">
                 <span className="close-icon" onClick={onClose}>&times;</span>
-                <h2>Employee Created!</h2>
+                <h2>Employé créé !</h2>
             </div>
         </div>
     );
