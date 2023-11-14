590001b4f8e6db1b2e65b95aac282d0826c135ab
kevin swp <thranva@gmail.com>
2023-11-14 16:44:54 +0100

    update varible text modal

--- dist/components/modal/index.js
+++ dist/components/modal/index.js
@@ -7,10 +7,17 @@ exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
 require("./modal.scss");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+/**
+ * Modal component for displaying a modal dialog.
+ * @param {boolean} isOpen - Determines if the modal is open or closed.
+ * @param {Function} onClose - Function to call when closing the modal.
+ * @param {string} modalText - Text to display in the modal.
+ */
 const Modal = _ref => {
   let {
     isOpen,
-    onClose
+    onClose,
+    modalText
   } = _ref;
   if (!isOpen) return null;
   return /*#__PURE__*/_react.default.createElement("div", {
@@ -20,6 +27,6 @@ const Modal = _ref => {
   }, /*#__PURE__*/_react.default.createElement("span", {
     className: "close-icon",
     onClick: onClose
-  }, "\xD7"), /*#__PURE__*/_react.default.createElement("h2", null, "Employee Created!")));
+  }, "\xD7"), /*#__PURE__*/_react.default.createElement("h2", null, modalText || 'Modifiez ce texte')));
 };
 var _default = exports.default = Modal;
--- src/lib/components/modal/index.jsx
+++ src/lib/components/modal/index.jsx
@@ -5,17 +5,16 @@ import './modal.scss';
  * Modal component for displaying a modal dialog.
  * @param {boolean} isOpen - Determines if the modal is open or closed.
  * @param {Function} onClose - Function to call when closing the modal.
+ * @param {string} modalText - Text to display in the modal.
  */
-const Modal = ({ isOpen, onClose }) => {
-    if (!isOpen) 
-    
-    return null;
+const Modal = ({ isOpen, onClose, modalText }) => {
+    if (!isOpen) return null;
 
     return (
         <div className="modal-overlay">
             <div className="modal-content">
                 <span className="close-icon" onClick={onClose}>&times;</span>
-                <h2>Employé créé !</h2>
+                <h2>{modalText || 'Modifiez ce texte'}</h2>
             </div>
         </div>
     );
